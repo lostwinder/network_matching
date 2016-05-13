89ebac1fa91f5a0c967cd6f6fedf411e01766a84
zhangzhehust <zhangzhe.hust@gmail.com>
2016-05-13 18:44:26 -0500

    refactor and add total job time calculation

--- experiment/total_time_calculate.py
+++ experiment/total_time_calculate.py
@@ -4,11 +4,8 @@ import subprocess
 
 num = int(raw_input("Enter the number for condor_history '-match' argument:"))
 job_ad_file = raw_input("Enter the filename for saving complete condor job classads:")
-job_start_date_file = raw_input("Enter the filename for saving the JobCurrentStartDate attribute values:")
-job_start_exec_date_file = raw_input("Enter the filename for saving the JobCurrentStartExecutingDate attribute values:")
 
 cwd = os.getcwd()
-print cwd + "/" + job_ad_file
 if os.path.exists(cwd + "/" + job_ad_file):
   pass
 else:
@@ -16,37 +13,30 @@ else:
   subprocess.call(["condor_history", "-match", str(num), "-long"], stdout=f)
   f.close()
 
-if os.path.exists(cwd + "/" + job_start_date_file):
-  pass
-else:
-  f1 = open(job_ad_file, "r")
-  f2 = open(job_start_date_file, "w")
-  f3 = open(job_start_exec_date_file, "w")
-  lines = f1.readlines()
-  for line in lines:
-    if "JobCurrentStartDate" in line:
-      f2.write(line)
-    if "JobCurrentStartExecutingDate" in line:
-      f3.write(line)
-  f1.close()
-  f2.close()
-  f3.close()
+qdate = []
+start_date = []
+start_exec_date = []
+completion_date = []
 
-exp_start_date = open(job_start_date_file, "r")
-lines = exp_start_date.readlines()
-exp_start_date_list = []
+f = open(job_ad_file, "r")
+lines = f.readlines()
 for line in lines:
-  exp_start_date_list.append(line.split(" ")[2][:-1])
+  if "QDate" in line:
+    qdate.append(line.split(" ")[2][:-1])
+  if "JobCurrentStartDate" in line:
+    start_date.append(line.split(" ")[2][:-1])
+  if "JobCurrentStartExecutingDate" in line:
+    start_exec_date.append(line.split(" ")[2][:-1])
+  if "CompletionDate" in line:
+    completion_date.append(line.split(" ")[2][:-1])
 
-exp_start_exec_date = open(job_start_exec_date_file, "r")
-lines = exp_start_exec_date.readlines()
-exp_start_exec_date_list = []
-for line in lines:
-  exp_start_exec_date_list.append(line.split(" ")[2][:-1])
 
-total_time = 0
+total_file_transfer_time = 0
+total_job_time = 0
 
-for i in range(len(exp_start_date_list)):
-  total_time = long(exp_start_exec_date_list[i]) - long(exp_start_date_list[i]) + total_time
+for i in range(num):
+  total_file_transfer_time += long(start_exec_date[i]) - long(start_date[i])
+  total_job_time += long(completion_date[i]) - long(qdate[i])
 
-print total_time
+print 'Total file transfer time is %ld.' % total_file_transfer_time
+print 'Total job time is %ld.' % total_job_time

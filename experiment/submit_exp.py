cb0517f681d3a9676d795712430a8cc55a59b74d
zhangzhehust <zhangzhe.hust@gmail.com>
2016-05-27 17:56:42 -0500

    refactor codes

---
+++ experiment/submit_exp.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+import os
+from random import shuffle
+from subprocess import call
+from time import sleep
+
+def generate_condor_submit_file(index, file_size):
+  filename = "condor_script_" + str(index)
+  condor_script = open(filename, "w")
+  condor_script.write("Universe = vanilla\n")
+  condor_script.write("Executable = /bin/pwd\n")
+  condor_script.write("Output = test.out."+str(index)+"\n")
+  condor_script.write("Error = test.err."+str(index)+"\n")
+  condor_script.write("Log = test.log."+str(index)+"\n")
+  condor_script.write("input = "+str(file_size)+"MB.zip\n")
+  condor_script.write("should_transfer_files = if_needed\n")
+  condor_script.write("when_to_transfer_output = on_exit\n")
+  condor_script.write("Queue")
+
+
+
+def main():
+  
+  # initialize some variables
+  # file_size:      store the list of unique input file sizes in ascending order
+  # job_histogram:  store the corresponding histogram for the input file sizes
+  # job_list:       the actual job list in submission order (input file size)
+  # index_list:     the index for the shuffled job list
+  # num_jobs:       the total number of jobs for this experiment
+
+  file_size = []
+  job_histogram = []
+  job_list = []
+  index_list = []
+  num_jobs = 0
+
+  run_no = int(raw_input("Index for this set of experiments: "))
+  need_submit_file = raw_input("Need generating submission files? (Y or N): ")
+  if need_submit_file == "Y":
+    # the raw input should be separated by space
+    file_size_str = raw_input("Unique file sizes in ascending order: ")
+    job_histogram_str = raw_input("Corresponding job histogram: ")
+
+    file_size = [int(i) for i in file_size_str.split(" ")]
+    job_histogram = [int(i) for i in job_histogram_str.split(" ")]
+
+    for i in range(len(file_size)):
+      for j in range(job_histogram[i]):
+        job_list.append(file_size[i])
+
+    num_jobs = len(job_list)
+    index_list = range(num_jobs)
+    shuffle(index_list)
+
+  elif need_submit_file == 'N':
+    # figure out the number of jobs based on the number of submission scripts
+    files = os.listdir(".")
+    for file in files:
+      if file.startswith("condor_script"):
+        num_jobs += 1
+
+  # submit 5 jobs per second
+  for i in range(num_jobs):
+    if (i+1)%5 == 1:
+      sleep(1)
+    if need_submit_file == "Y":
+      generate_condor_submit_file(i+1, job_list[index_list[i]])
+    call(["condor_submit", "condor_script_"+str(i+1)])
+
+if __name__ == "__main__":
+  main()
--- experiment/total_time_calculate.py
+++ experiment/total_time_calculate.py
@@ -2,25 +2,45 @@
 import os
 import subprocess
 
-num = int(raw_input("Enter the number for condor_history '-match' argument:"))
-job_ad_file = raw_input("Enter the filename for saving complete condor job classads:")
+num = int(raw_input("Number for condor_history '-match' argument: "))
+job_ad_file = raw_input("Filename for saving complete condor job classads: ")
+exp_result_file = raw_input("Filename for saving the experiment results: ")
 
 cwd = os.getcwd()
 if os.path.exists(cwd + "/" + job_ad_file):
+  # sometimes we just want to recalculate the time statistics  
+  # on some exisiting condor_history file, therefore no need to 
+  # run condor_history command and save to the same file again
   pass
 else:
   f = open(job_ad_file, "w")
   subprocess.call(["condor_history", "-match", str(num), "-long"], stdout=f)
   f.close()
 
+cluster_id = []
+execution_site = []
+transfer_input_size = []
 qdate = []
 start_date = []
 start_exec_date = []
 completion_date = []
 
+# key: ClusterId
+# value: a list of attributes, the order of attributes are as following
+# 1:ExecutionSite         2:TransferInputSizeMB          3:QDate 
+# 4:JobCurrentStartDate   5:JobCurrentStartExecutingDate 6:JobCompletionDate 
+# 7:ActualTransferTime    8:EstimatedTransferTime        9:MachineSlot
+time_stat_dict = {}
+
 f = open(job_ad_file, "r")
 lines = f.readlines()
 for line in lines:
+  if "ClusterId" in line:
+    cluster_id.append(line.split(" ")[2][:-1])
+  if "MATCH_EXP_Glidein_CMSSite" in line:
+    execution_site.append(line.split(" ")[2][:-1])
+  if "TransferInputSizeMB" in line:
+    transfer_input_size.append(line.split(" ")[2][:-1])
   if "QDate" in line:
     qdate.append(line.split(" ")[2][:-1])
   if "JobCurrentStartDate" in line:
@@ -30,6 +50,18 @@ for line in lines:
   if "CompletionDate" in line:
     completion_date.append(line.split(" ")[2][:-1])
 
+for i in range(num):
+  key = cluster_id[i]
+  time_stat_dict[key] = []
+  time_stat_dict[key].append(execution_site[i])
+  time_stat_dict[key].append(transfer_input_size[i])
+  time_stat_dict[key].append(qdate[i])
+  time_stat_dict[key].append(start_date[i])
+  time_stat_dict[key].append(start_exec_date[i])
+  time_stat_dict[key].append(completion_date[i])
+  actual_transfer_time = long(start_exec_date[i]) - long(start_date[i])
+  time_stat_dict[key].append(str(actual_transfer_time))
+    
 
 total_file_transfer_time = 0
 total_job_time = 0
@@ -40,3 +72,53 @@ for i in range(num):
 
 print 'Total file transfer time is %ld.' % total_file_transfer_time
 print 'Total job time is %ld.' % total_job_time
+
+# process condor negotiator log for estimated file tranfer time in  matchmaking
+# first process NegotiatorLog.old, then NegotiatorLog
+negotiator_log = "/var/log/condor/NegotiatorLog"
+negotiator_log_old = "/var/log/condor/NegotiatorLog.old"
+grep_output = str()
+
+if os.path.exists(negotiator_log_old):
+  grep_output += subprocess.Popen(['grep', 'Exp:', negotiator_log_old], 
+                                    stdout=subprocess.PIPE).communicate()[0]
+if os.path.exists(negotiator_log):
+  grep_output += subprocess.Popen(['grep', 'Exp:', negotiator_log], 
+                                    stdout=subprocess.PIPE).communicate()[0]
+
+grep_output = grep_output.split('\n')
+# remove the last empty string
+grep_output = grep_output[:-1]
+# only use the last num_of_jobs entries of the grep output
+estimation_info = grep_output[-num:]
+for line in estimation_info:
+  columns = line.split(",")
+  cluster_id = columns[0].split(" ")[-1]
+  estimated_transfer_time = columns[2].split(" ")[-1]
+  execution_slot = columns[3].split(" ")[-1]
+  time_stat_dict[cluster_id].append(estimated_transfer_time)
+  time_stat_dict[cluster_id].append(execution_slot)
+
+
+# write the actual and estimated file transfer information to CSV file
+# the columns are arranged in the following structure
+# 1:ClusterId          2:ExecutionSite          3:TransferInputSizeMB
+# 4:QDate              5:JobCurrentStartDate    6:JobCurrentStartExecutingDate 
+# 7:JobCompletionDate  8:ActualTransferTime     9:EstimatedTransferTime
+# 10:MachineSlot
+
+f = open(exp_result_file, 'w')
+f.write("ClusterId" + " " + "ExecutionSite" + " " + "TransferInputSizeMB" + " "
+        + "QDate" + " " + "JobCurrentStartDate" + " " + 
+	"JobCurrentStartExecutingDate" + " " + "JobCompletionDate" + " " +
+	"ActualTransferTime" + " " + "EstimatedTransferTime" + " " + 
+	"MachineSlot" + "\n")
+for key in sorted(time_stat_dict):
+  line = key
+  for attr in time_stat_dict[key]:
+    line += " "
+    line += attr
+  f.write(line + '\n')
+
+f.write("Total_file_transfer_time: " + str(total_file_transfer_time) + "\n")
+f.write("Total_job_time: " + str(total_job_time) + "\n")
